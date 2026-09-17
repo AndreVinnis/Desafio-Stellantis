87ce573b1b24cd1fc7d040c31b876482ecd75bc8
AndreVinnis <andrev.macambira@gmail.com>
2026-09-17 11:42:54 -0300

    chore: criando ambiente docker

---
+++ backend/src/main/java/com/andre/DesafioStellantis/DesafioStellantisApplication.java
@@ -0,0 +1,13 @@
+package com.andre.DesafioStellantis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioStellantisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioStellantisApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/andre/DesafioStellantis/DesafioStellantisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.DesafioStellantis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioStellantisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
